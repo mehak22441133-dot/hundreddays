d39111d4a8a87fc30e86c325e8320648192d3a20
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-19 21:49:32 +0530

    Update day35a.c
    Updated the array rotation logic and added user prompts.

--- day35a.c
+++ day35a.c
@@ -1,40 +1,36 @@
 //Q70: Rotate an array to the right by k positions.
-
-/*
-Sample Test Cases:
-Input 1:
-5
-1 2 3 4 5
-2
-Output 1:
-4 5 1 2 3
-
-*/  
 #include <stdio.h>
+
 int main() {
-    int n;
+    int n, k, i;
+    int arr[100], temp[100];
+
+    printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
-    
-    int arr[n];
-    for (int i = 0; i < n; i++) {
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    
-    int k;
+
+    printf("Enter number of positions to rotate (k): ");
     scanf("%d", &k);
-    k = k % n; // In case k is greater than n
-    
-    // Rotate the array
-    int rotated[n];
-    for (int i = 0; i < n; i++) {
-        rotated[(i + k) % n] = arr[i];
+
+    k = k % n;
+
+    for (i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
     }
-    
-    // Print the rotated array
-    for (int i = 0; i < n; i++) {
-        printf("%d ", rotated[i]);
+
+    for (i = 0; i < n; i++) {
+        arr[i] = temp[i];
     }
+
+    printf("Array after rotating right by %d positions:\n", k);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
     printf("\n");
-    
     return 0;
 }
