c03fc046ef898a3e95f3734368b61850ffd7845d
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-19 22:23:10 +0530

    Refactor second largest element finder with user prompts
    Updated the program to include prompts for user input and improved handling of the second largest element case.

--- day35.c
+++ day35.c
@@ -1,27 +1,39 @@
 //Q69: Find the second largest element in an array.
+//Q69: Find the second largest element in an array.
+
 #include <stdio.h>
 
 int main() {
     int n;
+
+    printf("Enter the size of the array: ");
     scanf("%d", &n);
 
     int arr[n];
-    for(int i = 0; i < n; i++) {
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    int largest = -1e9, second_largest = -1e9;
+    int largest = arr[0];
+    int second = -2147483648;  
 
-    for(int i = 0; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         if (arr[i] > largest) {
-            second_largest = largest;
+            second = largest;
             largest = arr[i];
-        } 
-        else if (arr[i] > second_largest && arr[i] != largest) {
-            second_largest = arr[i];
+        } else if (arr[i] > second && arr[i] != largest) {
+            second = arr[i];
         }
     }
 
-    printf("%d\n", second_largest);
+    if (second == -2147483648) {
+        printf("There is no second largest element (all elements may be equal).\n");
+    } else {
+        printf("Second largest element = %d\n", second);
+    }
+
     return 0;
 }
+
