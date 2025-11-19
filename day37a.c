f3a00ca1d7db9c03aa6e73bc4b1e782e1f01e688
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-19 22:59:35 +0530

    update day37a.c
    Updated matrix size to fixed dimensions and improved code structure.

--- day37a.c
+++ day37a.c
@@ -1,28 +1,34 @@
 //Q74: Find the transpose of a matrix.
-
 #include <stdio.h>
-int main() {
 
+int main() {
     int rows, cols;
     scanf("%d %d", &rows, &cols);
-    
-    int matrix[rows][cols];
+
+    int matrix[100][100], transpose[100][100];
+
+    // Input matrix
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    int tp[cols][rows];
+
+    // Transpose logic
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            tp[j][i] = matrix[i][j];
+            transpose[j][i] = matrix[i][j];
         }
     }
+
+    // Output transpose
     for (int i = 0; i < cols; i++) {
         for (int j = 0; j < rows; j++) {
-            printf("%d ", tp[i][j]);
+            printf("%d ", transpose[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
+
