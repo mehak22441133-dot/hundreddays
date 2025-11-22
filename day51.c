558d4f77dc5b578bc5ab0c46355451805c43a602
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-11-22 22:37:08 +0530

    Optimize target search with binary search method
    Refactor program to find first and last occurrences of a target in a sorted array using binary search.

--- day51.c
+++ day51.c
@@ -1,43 +1,60 @@
 //Q101: Write a Program to take a sorted array(say nums[]) and an integer (say target) as inputs. The elements in the sorted array might be repeated. You need to print the first and last occurrence of the target and print the index of first and last occurrence. Print -1, -1 if the target is not present.
 
-/*
-Sample Test Cases:
-Input 1:
-nums = [5,7,7,8,8,10], target = 8
-Output 1:
-3,4
-
-Input 2:
- nums = [5,7,7,8,8,10], target = 6
-Output 2:
--1,-1
-
-Input 3:
- nums = [5,7,7,8,8,10], target = 10
-Output 3:
-5,5
-
-*/
 #include <stdio.h>
-int main() {
-    int n, target;
-    scanf("%d", &n);
-    int nums[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &nums[i]);
-    }
-    scanf("%d", &target);
 
-    int first = -1, last = -1;
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == target) {
-            if (first == -1) {
-                first = i;
-            }
-            last = i;
+void findFirstLast(int nums[], int n, int target) {
+    int f= -1, la= -1;
+    
+    // Find first occurrence
+    int lf = 0, rg = n - 1;
+    while (lf <= rg) {
+        int mid = lf + (rg - lf) / 2;
+        if (nums[mid] == target) {
+            f = mid;
+            rg = mid - 1; // Continue searching left
+        } else if (nums[mid] < target) {
+            lf = mid + 1;
+        } else {
+            rg = mid - 1;
         }
     }
+    
+    // If target not found
+    if (f == -1) {
+        printf("-1,-1\n");
+        return;
+    }
+    
+    // Find last occurrence
+    int lf = 0, rg = n - 1;
+    while (lf <= rg) {
+        int mid = lf + (rg - lf) / 2;
+        if (nums[mid] == target) {
+            la = mid;
+            lf = mid + 1; // Continue searching right
+        } else if (nums[mid] < target) {
+            lf = mid + 1;
+        } else {
+            rg = mid - 1;
+        }
+    }
+    
+    printf("%d,%d\n", f, la);
+}
 
-    printf("%d,%d\n", first, last);
+int main() {
+    int nums[] = {5,7,7,8,8,10};
+    int n = sizeof(nums) / sizeof(nums[0]);
+    
+    // Test cases
+    printf("Target 8: ");
+    findFirstLast(nums, n, 8);  // Output: 3,4
+    
+    printf("Target 6: ");
+    findFirstLast(nums, n, 6);  // Output: -1,-1
+    
+    printf("Target 10: ");
+    findFirstLast(nums, n, 10); // Output: 5,5
+    
     return 0;
 }
