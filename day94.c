371ad5475d594f60102fe53d721bda6e8266057c
mehak22441133-dot <mehaknegi2802@gmail.com>
2025-12-02 01:39:52 +0530

    Remove commented-out code and clean up formatting

--- day94.c
+++ day94.c
@@ -1,13 +1,6 @@
 //Q144: Write a function that accepts a structure as parameter and prints its members.
 
-/*
-Sample Test Cases:
-Input 1:
-Student: Neha 104 92
-Output 1:
-Name: Neha | Roll: 104 | Marks: 92
 
-*/
 #include <stdio.h>
 struct Student {
     char name[50];
@@ -29,3 +22,4 @@ int main() {
     printStudent(student);
     return 0;
 }
+
